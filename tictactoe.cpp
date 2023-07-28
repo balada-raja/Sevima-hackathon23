1498f108a83f369a73f7a3eb24232bd362d49063
balada-raja <barabaskara28@gmail.com>
2023-07-28 08:44:17 +0700

    perbaikan bug

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-char square [10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char square [10] = {' ', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 //function to return the game status
 
@@ -28,14 +28,19 @@ int checkwin(){
             && square[7] != '7' && square [8] != '8' && square[9] != '9'){
         return 0;
     }else{
-        return 0;
+        return -1;
     }
+}
+
+void clearScreen() {
+    // Use ANSI escape sequence to clear the screen
+    cout << "\x1B[2J\x1B[H";
 }
 
 //this function will draw rhe board with the player
 
 void board(){
-    system("cls");
+    clearScreen();
     cout << "\n\n Tic Tac Toe Game \n\n";
 
     cout << "playar 1 (X) and player 2(O)\n" << endl;
@@ -59,48 +64,43 @@ int main(){
 
     char mark;
 
-    do{
+    int result;
+    do {
         board();
-        player = (player%2)?1:2;
+        player = (player % 2) ? 1 : 2;
 
-        cout << "player " << player << ", enter the number : ";
+        cout << "Player " << player << ", enter the number : ";
         cin >> choice;
 
         mark = (player == 1) ? 'X' : 'O';
 
-        if(choice == 1 && square [1] == '1'){
-            square[1] = mark;
-        }else if (choice == 2 && square [2] == '2'){
-            square[2] = mark;
-        }else if (choice == 3 && square [3] == '3'){
-            square[3] = mark;
-        }else if (choice == 4 && square [4] == '4'){
-            square[4] = mark;
-        }else if (choice == 5 && square [5] == '5'){
-            square[5] = mark;
-        }else if (choice == 6 && square [6] == '6'){
-            square[6] = mark;
-        }else if (choice == 7 && square [7] == '7'){
-            square[7] = mark;
-        }else if (choice == 8 && square [8] == '8'){
-            square[8] = mark;
-        }else if (choice == 9 && square [9] == '9'){
-            square[9] = mark;
-        }else{
-            cout << "INVALID MOVE!";
-
-            player--;
+        // periksa input yang tidak valid
+        if (choice < 1 || choice > 9) {
+            cout << "Invalid move! Please enter a number between 1 and 9." << endl;
             cin.ignore();
             cin.get();
+            continue;
         }
 
-        i = checkwin();
+        // periksa apakah posisi sudah diisi sebelumnya
+        if (square[choice] != 'X' && square[choice] != 'O') {
+            square[choice] = mark;
+        } else {
+            cout << "Invalid move! That position is already taken." << endl;
+            cin.ignore();
+            cin.get();
+            continue;
+        }
+
+        result = checkwin();
         player++;
-    }while (i == -1);
+    } while (result == -1);
+
     board();
-    if( i == 1){
-        cout << "\a CONGRATULATION! PLAYER "<< --player << " WINS!";
-    }else{
+    if (result == 1) {
+        // pemain yang menang adalah player sebelumnya
+        cout << "\a CONGRATULATION! PLAYER " << player - 1 << " WINS!";
+    } else {
         cout << "\a DRAW!";
     }
 
@@ -108,3 +108,53 @@ int main(){
     cin.get();
     return 0;
 }
+
+//     do{
+//         board();
+//         player = (player%2)?1:2;
+
+//         cout << "player " << player << ", enter the number : ";
+//         cin >> choice;
+
+//         mark = (player == 1) ? 'X' : 'O';
+
+//         if(choice == 1 && square [1] == '1'){
+//             square[1] = mark;
+//         }else if (choice == 2 && square [2] == '2'){
+//             square[2] = mark;
+//         }else if (choice == 3 && square [3] == '3'){
+//             square[3] = mark;
+//         }else if (choice == 4 && square [4] == '4'){
+//             square[4] = mark;
+//         }else if (choice == 5 && square [5] == '5'){
+//             square[5] = mark;
+//         }else if (choice == 6 && square [6] == '6'){
+//             square[6] = mark;
+//         }else if (choice == 7 && square [7] == '7'){
+//             square[7] = mark;
+//         }else if (choice == 8 && square [8] == '8'){
+//             square[8] = mark;
+//         }else if (choice == 9 && square [9] == '9'){
+//             square[9] = mark;
+//         }else{
+//             cout << "INVALID MOVE!";
+
+//             player--;
+//             cin.ignore();
+//             cin.get();
+//         }
+
+//         i = checkwin();
+//         player++;
+//     }while (i == -1);
+//     board();
+//     if( i == 1){
+//         cout << "\a CONGRATULATION! PLAYER "<< --player << " WINS!";
+//     }else{
+//         cout << "\a DRAW!";
+//     }
+
+//     cin.ignore();
+//     cin.get();
+//     return 0;
+// }
